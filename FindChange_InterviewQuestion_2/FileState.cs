f3c01ea24376aad4b8c4a78b8beba320f12601f2
Matan <matan2050@gmail.com>
2016-03-05 15:30:16 +0200

    Initial commit

---
+++ FindChange_InterviewQuestion_2/FileState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FindChange_InterviewQuestion_2
+{
+    public class FileState
+    {
+        #region FIELDS
+        private     string      pathToFile;
+        private     long        fileSizeBytes;
+        private     int         stateResolution;
+        #endregion
+
+
+        #region CONSTRUCTOR
+        public FileState(string _pathToFile, int _stateResolution)
+        {
+            pathToFile = _pathToFile;
+
+            FileInfo f = new FileInfo(pathToFile);
+
+            fileSizeBytes = f.Length;
+
+            stateResolution = _stateResolution;
+        }
+        #endregion
+
+
+        #region PUBLIC_METHODS
+        public void FreezeState()
+        {
+
+        }
+        #endregion
+
+
+        #region PRIVATE_METHODS
+
+        #endregion
+    }
+}
