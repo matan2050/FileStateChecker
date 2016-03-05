0af69da6eeec53fd6b8608fc2dfca3cd6b0c7fe7
Matan <matan2050@gmail.com>
2016-03-05 19:52:22 +0200

    Changed FileState to generate file with {start index, end index, md5 hash}

--- FindChange_InterviewQuestion_2/FileState.cs
+++ FindChange_InterviewQuestion_2/FileState.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.IO;
+using System.Text;
 
 namespace FindChange_InterviewQuestion_2
 {
     public class FileState
     {
         #region FIELDS
-        private     string      pathToFile;
-        private     long        fileSizeBytes;
-        private     int         stateResolution;
+        private     string      pathToFile;             // Path to the file we want to track
+        private     string      pathToStateFile;        // Path to the file that holds the state for tracked file
+        private     long        fileSizeBytes;          // Size of that file in bytes
+        private     int         stateResolution;        // Resolution to detect change in bytes
         #endregion
 
 
@@ -20,6 +20,7 @@ namespace FindChange_InterviewQuestion_2
         public FileState(string _pathToFile, int _stateResolution)
         {
             pathToFile = _pathToFile;
+            pathToStateFile = ReplaceExtension(pathToFile);
 
             FileInfo f = new FileInfo(pathToFile);
 
@@ -31,15 +32,74 @@ namespace FindChange_InterviewQuestion_2
 
 
         #region PUBLIC_METHODS
-        public void FreezeState()
+        /// <summary>
+        /// Creates a state file for the tracked file.
+        /// state is basically a table with byte index in tracked file with the value in that byte
+        /// if neighbouring bytes are of the same value, function goesin each direction and searches
+        /// for the first non equal byte
+        /// </summary>
+        public void GenerateStateFile()
         {
+            long        startPos            = 0;
+            long        endPos              = fileSizeBytes;
+            long        localStartPos;
+            long        localEndPos;
 
+
+            using (var fileStateStream = new StreamWriter(pathToStateFile))
+            {
+                using (var fileStream = new FileStream(pathToFile, FileMode.Open))
+                {
+                    for (long i = startPos; i <= endPos; i = i + stateResolution)
+                    {
+                        localStartPos = i;
+                        localEndPos = i + stateResolution - 1;
+
+                        byte[]      currReadBuffer      = new byte[localEndPos - localStartPos];
+
+                        fileStream.Read(currReadBuffer, 0, (int)(localEndPos - localStartPos));
+
+                        byte[]      currBufferHash      = GenerateHash(currReadBuffer);
+                        string      hashAsString        = Encoding.UTF8.GetString(currBufferHash);
+
+                        string currBufferOutputStream = localStartPos.ToString() + "," + localEndPos.ToString() + "," +
+                                                    hashAsString;
+
+                        byte[] currBufferOutputArray = Encoding.ASCII.GetBytes(currBufferOutputStream);
+
+
+                        fileStateStream.WriteLine(currBufferOutputStream);
+                    }
+                }
+            }
         }
         #endregion
 
 
         #region PRIVATE_METHODS
+        private string ReplaceExtension(string filePath)
+        {
+            string      replacedString;
+            int         dotPosition         = filePath.IndexOf('.');
+            string      extension           = filePath.Substring(dotPosition+1, 3);
 
+            replacedString = filePath.Replace(extension, "stt");
+
+            return replacedString;
+        }
+
+        private byte[] GenerateHash(byte[] fileContentsBuffer)
+        {
+            byte[] hashCode;
+
+            using (var md5 = MD5.Create())
+            {
+                hashCode = md5.ComputeHash(fileContentsBuffer);
+            }
+
+            return hashCode;
+        }
         #endregion
+
     }
 }
