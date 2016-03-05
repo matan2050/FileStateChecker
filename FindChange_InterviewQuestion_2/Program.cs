e1af5bf67476c1f3d85cf52b51441d7d04b53340
Matan <matan2050@gmail.com>
2016-03-05 22:15:42 +0200

    Changed program.cs to generate state file for 2 slightly different files (for diff)

--- FindChange_InterviewQuestion_2/Program.cs
+++ FindChange_InterviewQuestion_2/Program.cs
@@ -13,6 +13,10 @@ namespace FindChange_InterviewQuestion_2
             string pathToFile = @"C:\Users\User\Desktop\InterviewQuestions\classifierHOG.mat";
             FileState fs = new FileState(pathToFile, 4 * 1024 * 1024);
             fs.GenerateStateFile();
+
+            string pathToSecondFile = @"C:\Users\User\Desktop\InterviewQuestions\classifierHOG - Copy.mat";
+            FileState fs2 = new FileState(pathToSecondFile, 4 * 1024 * 1024);
+            fs2.GenerateStateFile();
         }
     }
 }
