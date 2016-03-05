f8f195e705bf5eaa59b66b02f5c17d98820d2093
Matan <matan2050@gmail.com>
2016-03-05 23:55:41 +0200

    Updated paths to tracked files

--- FindChange_InterviewQuestion_2/Program.cs
+++ FindChange_InterviewQuestion_2/Program.cs
@@ -10,13 +10,16 @@ namespace FindChange_InterviewQuestion_2
     {
         static void Main(string[] args)
         {
-            string pathToFile = @"C:\Users\User\Desktop\InterviewQuestions\classifierHOG.mat";
+            string pathToFile = @"C:\Users\User\Desktop\InterviewQuestions\classifierHOG1.mat";
             FileState fs = new FileState(pathToFile, 4 * 1024 * 1024);
             fs.GenerateStateFile();
+            List<byte[]>  origHash = fs.ReadStateFile();
 
-            string pathToSecondFile = @"C:\Users\User\Desktop\InterviewQuestions\classifierHOG - Copy.mat";
+            string pathToSecondFile = @"C:\Users\User\Desktop\InterviewQuestions\classifierHOG - Copy1.mat";
             FileState fs2 = new FileState(pathToSecondFile, 4 * 1024 * 1024);
             fs2.GenerateStateFile();
+            List<byte[]>  editedHash = fs2.ReadStateFile();
+
         }
     }
 }
